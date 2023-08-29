d5f0639a2d829e573ea489bfe8d441e0e33939d5
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-08-30 00:23:23 +0100

    Binary Trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,8 +8,9 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
+	new = binary_tree_node(parent, value);
 	if (!parent)
 		return (NULL);
 	if (!new)
